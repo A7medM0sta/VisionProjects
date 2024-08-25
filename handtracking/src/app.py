19d1b52921933c28462a124f5a7523fc0bdda17c
ahmed mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2024-08-25 19:17:23 +0300

    new files

--- handtracking/src/app.py
+++ handtracking/src/app.py
@@ -9,9 +9,9 @@ from collections import deque
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-from handtracking.utils.cvfpscalc import CvFpsCalc
-from model import KeyPointClassifier
-from model import PointHistoryClassifier
+from handtracking.src.utils.cvfpscalc import CvFpsCalc
+from handtracking.src.model import KeyPointClassifier
+from handtracking.src.model import PointHistoryClassifier
 
 
 def get_args():
--- handtracking/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from handtracking.utils.cvfpscalc import CvFpsCalc
